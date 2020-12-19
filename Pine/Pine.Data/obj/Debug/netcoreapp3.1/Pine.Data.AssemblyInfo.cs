8165f0035ce5cfe5d872606a408e845658b19a1d
Mario Toshev <mario.a.toshev@gmail.com>
2020-12-19 16:48:01 +0200

    Fixes
    Removed old namespaces (DeriliumPoject -> PIne)

--- Pine/Pine.Data/obj/Debug/netcoreapp3.1/Pine.Data.AssemblyInfo.cs
+++ Pine/Pine.Data/obj/Debug/netcoreapp3.1/Pine.Data.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pine.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
