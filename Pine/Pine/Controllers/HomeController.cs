0be8ba65a5748f64431d6875fb661370b0a5633e
radisredkov <64905410+radisredkov@users.noreply.github.com>
2021-01-19 17:51:47 +0200

    added custom 404 page

--- Pine/Pine/Controllers/HomeController.cs
+++ Pine/Pine/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Pine.Models;
@@ -22,6 +23,13 @@ namespace Pine.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [Route("{*url}", Order = 999)]
+        public IActionResult CatchAll()
+        {
+            Response.StatusCode = 404;
+            return View("Views/Shared/404NotFound.cshtml");
         }
     }
 }
