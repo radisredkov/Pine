fb866a8545d15d4a3b7584d3a533b70615218b41
Mario Toshev <mario.a.toshev@gmail.com>
2021-01-28 16:17:08 +0200

    Creating RolesIs Now possible

---
+++ Pine/Pine.Models/Administration/CreateRoleViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Pine.Models.Administration
+{
+   public class CreateRoleViewModel
+    {
+        [Required]
+        public string roleName { get; set; }
+    }
+}
---
+++ Pine/Pine/Controllers/AdministrationController1.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Pine.Models.Administration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pine.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole
+                {
+                    Name = model.roleName
+                };
+            
+               IdentityResult result = await roleManager.CreateAsync(identityRole);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Community", "AllPosts");
+                }
+                foreach (var err in result.Errors)
+                {
+                    ModelState.AddModelError("", err.Description);
+                }
+            }    
+            return View(model );
+        }
+    }
+}
